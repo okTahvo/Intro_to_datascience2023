41d65fd1ae527a748a1bf7b23d8e518571d8bf0b
Jatynjala <johannestynjala@gmail.com>
2023-10-24 13:05:01 +0300

    radar graph drawing finished

--- data_interaction.py
+++ data_interaction.py
@@ -7,8 +7,10 @@ def draw_radar_graph(data):
     c = data["RAM to price"].mean()
     d = data["GPU memory to price"].mean()
     e = data["pixel count to price"].mean()
-    print({'clock speed':a, 'drive capacity':b, 'RAM':c, 'GPU memory':d, 'pixel count':e})
-    dataf = pd.DataFrame(dict(r=[a, b, c, d, e], theta=['clock speed', 'drive capacity', 'RAM', 'GPU memory', 'pixel count']))
+    f = data["screen size to price"].mean()
+    g = data["weight to price"].mean()
+    #print({'clock speed score':a, 'drive capacity score':b, 'RAM score':c, 'GPU memory score':d, 'pixel count score':e, 'screen size score':f, 'weight score':g})
+    dataf = pd.DataFrame(dict(r=[a, b, c, d, e, f, g], theta=['clock speed', 'drive capacity', 'RAM', 'GPU memory', 'pixel count', 'screen size', 'weight']))
     fig = px.line_polar(dataf, r='r', theta='theta', line_close=True)
     fig.update_traces(fill='toself')
     fig.show()
@@ -32,9 +34,46 @@ def filter_data(data):
         return (data, False)
     return (data, True)
 
+def add_entry():
+    entry = {}
+    entry["brand"] = input("Please enter laptop brand: ")
+    entry["price"] = int(input("Please enter price in Indian rupees: "))
+    entry["processor brand"] = input("Please enter processor brand: ")
+    entry["processor number"] = int(input("Please enter processor number (e.g. Intel i9 = 9): "))
+    entry["processor year"] = int(input("Please enter processor year: "))
+    entry["processor clock speed"] = float(input("Please enter maximum processor clock speed (GHz): "))
+    entry["processor core count"] = int(input("Please enter number of processor cores: "))
+    a = input("Does the laptop have SSD? Any answer other than 'Yes' will be interpreted as 'No'. Answer: ")
+    if a == "Yes":
+        entry["SSD"] = True
+    else:
+        entry["SSD"] = False
+    entry["drive capacity"] = int(input("Please enter drive capacity (GB): "))
+    entry["RAM"] = int(input("Please enter RAM capacity (GB): "))
+    entry["GPU memory"] = int(input("Please enter dedicated GPU capacity (GB): "))
+    entry["screen size"] = input("Please enter screen size in inches: ")
+    entry["pixel count"] = int(input("Please enter total number of pixels: "))
+    entry["weight"] = input("Please enter weight (kg): ")
+    for item in ["speakers", "touchscreen", "microphone", "webcam"]:
+        mes = "Does the laptop have "+item+"? Any answer other than 'Yes' will be interpreted as 'No'. Answer: "
+        a = input(mes)
+        if a == "Yes":
+            entry[item] = True
+        else:
+            entry[item] = False
+    return entry
+
+def convert_to_float(value):
+    try:
+        return float(value)
+    except:
+        return None
+
 if __name__ == '__main__':
-    df = pd.read_csv("preprocessed_laptop_data.csv")
-    for field in ["processor clock speed", "drive capacity", "RAM", "GPU memory", "pixel count"]:
+    df = pd.read_csv("preprocessed_laptop_data.csv")#read data
+    df["screen size"] = df["screen size"].apply(convert_to_float)
+    df["weight"] = df["weight"].apply(convert_to_float)
+    for field in ["processor clock speed", "drive capacity", "RAM", "GPU memory", "pixel count", "screen size", "weight"]:#compute scores for features
         df[field] = df[field].fillna(df[field].mean())
         name = field+" to price"
         df[name] = (df[field])/(df["price"])
@@ -46,17 +85,21 @@ if __name__ == '__main__':
         print("What would you like me to do?")
         print("1: Draw radar chart")
         print("2: Filter data")
+        print("3: Add a new entry")
         print("q: quit")
         answer = input("answer: ")
-        if answer == "1":
+        if answer == "1":#draw radar graph
             draw_radar_graph(df2)
-        elif answer == "2":
+        elif answer == "2":#filter data
             df2 = df.copy()
             success = filter_data(df2)
             if success[1] == False:
                 print("No matching data")
             else:
                 df2 = success[0]
-        elif answer == "q":
+        elif answer == "3":#create a new entry (not written to any dataframes or databases)
+            entry = add_entry()
+            print(entry)
+        elif answer == "q":#exit
             print("Have a nice day!")
             break
