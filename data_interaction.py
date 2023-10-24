e1583066e31e57bc45ca14e715794acb2e44cfde
Jatynjala <johannestynjala@gmail.com>
2023-10-24 11:24:51 +0300

    interaction program updated to include data filtering

--- data_interaction.py
+++ data_interaction.py
@@ -13,14 +13,34 @@ def draw_radar_graph(data):
     fig.update_traces(fill='toself')
     fig.show()
 
+def filter_data(data):
+    for fiel in ["brand", "processor brand"]:
+        values = sorted(data[fiel].unique())
+        print(values)
+        an = input("Please write one of the items in the list above. Any other answer skips: ")
+        if an in values:
+            data = data[data[fiel]==an]
+    for field in ["webcam", "microphone", "touchscreen", "speakers", "SSD"]:
+        message = "Would you like to have "+field+"?"
+        print(message)
+        ans = input("Yes/No. Any other answer skips: ")
+        if ans == "Yes":
+            data = data[data[field]==True]
+        elif ans == "No":
+            data = data[data[field]==False]
+    if data.shape[0] == 0:
+        return (data, False)
+    return (data, True)
+
 if __name__ == '__main__':
     df = pd.read_csv("preprocessed_laptop_data.csv")
     for field in ["processor clock speed", "drive capacity", "RAM", "GPU memory", "pixel count"]:
+        df[field] = df[field].fillna(df[field].mean())
         name = field+" to price"
         df[name] = (df[field])/(df["price"])
         maxi = df[name].max()
         df[name] = (df[name])/maxi
-    print(df.head())
+    df2 = df.copy()
     print("Hello! I am an interaction tool for the data.")
     while True:
         print("What would you like me to do?")
@@ -29,9 +49,14 @@ if __name__ == '__main__':
         print("q: quit")
         answer = input("answer: ")
         if answer == "1":
-            draw_radar_graph(df)
+            draw_radar_graph(df2)
         elif answer == "2":
-            pass
+            df2 = df.copy()
+            success = filter_data(df2)
+            if success[1] == False:
+                print("No matching data")
+            else:
+                df2 = success[0]
         elif answer == "q":
             print("Have a nice day!")
             break
